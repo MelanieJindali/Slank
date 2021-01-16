5d17422662ab90fca9d1ae6cf52e5106ca000532
Melanie Jindali <melaniejindali.dm@gmail.com>
2021-01-16 14:39:52 -0600

    Adding SPDJ imgs and blurb placeholders

--- slank-official/frontend/src/screens/components/SPDJ/index.js
+++ slank-official/frontend/src/screens/components/SPDJ/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React from 'react'
+import SPDJ1 from './SPDJ1.jpg'
+import SPDJ2 from './SPDJ2.JPG'
 
 const SPDJ = () => {
-  return (
-    <div>
-      <h1>Making music with the locals</h1>
-      <p>Here is a blurb about SPDJ and Slank's contribution</p>
-    </div>
-  );
-};
+	return (
+		<div className='spdj'>
+			<div className='row'>
+				<div className='spdj-text'>
+					<h1>Making music with the locals</h1>
+					<p>Here is a blurb about SPDJ and Slank's contribution</p>
+				</div>
+				<img src={SPDJ1} alt='SPDJ club' />
+			</div>
+			<div className='row'>
+        <img src={SPDJ2} alt='SPDJ club' />
+				<div className='spdj-text'>
+					<h1>Making music with the locals</h1>
+					<p>Here is a blurb about SPDJ and Slank's contribution</p>
+				</div>
+				
+			</div>
+		</div>
+	)
+}
 
-export default SPDJ;
+export default SPDJ
--- slank-official/frontend/src/screens/sections/About/index.js
+++ slank-official/frontend/src/screens/sections/About/index.js
@@ -4,30 +4,33 @@ import SPDJ from '../../components/SPDJ'
 
 const About = () => {
 	return (
-		<div className='wrapper about' id='about'>
-			<img src={Slank} className='about-img' alt='Slank DJing' />
-			<div className='about-desc'>
-				<h1>About</h1>
-				<p>
-					Slank is a producer, engineer, and DJ based in Madison, Wisconsin.
-					Beginning in 2017 Slank’s musical career took off as a producer, and
-					by 2018 he had established himself as a well known DJ and entertainer.
-					<br />
-					<br />
-					From opening shows for artists like Borgore, Dombresky and
-					Hieroglyphics, to hosting events at weddings, local underground
-					performances, and UW-Madison club events, Slank sets the atmosphere in
-					all occasions in between.
-					<br />
-					<br />
-					As an innovative artist, Slank’s creativity extends beyond the
-					turntables as he produces his own music, music videos, and
-					merchandise. He strives to collaborate with artists in his community
-					and even hosts workshops teaching others how to DJ and produce.
-				</p>
-        <SPDJ />
+		<>
+			<div className='wrapper about' id='about'>
+				<img src={Slank} className='about-img' alt='Slank DJing' />
+				<div className='about-desc'>
+					<h1>About</h1>
+					<p>
+						Slank is a producer, engineer, and DJ based in Madison, Wisconsin.
+						Beginning in 2017 Slank’s musical career took off as a producer, and
+						by 2018 he had established himself as a well known DJ and
+						entertainer.
+						<br />
+						<br />
+						From opening shows for artists like Borgore, Dombresky and
+						Hieroglyphics, to hosting events at weddings, local underground
+						performances, and UW-Madison club events, Slank sets the atmosphere
+						in all occasions in between.
+						<br />
+						<br />
+						As an innovative artist, Slank’s creativity extends beyond the
+						turntables as he produces his own music, music videos, and
+						merchandise. He strives to collaborate with artists in his community
+						and even hosts workshops teaching others how to DJ and produce.
+					</p>
+				</div>
 			</div>
-		</div>
+			<SPDJ />
+		</>
 	)
 }
 
