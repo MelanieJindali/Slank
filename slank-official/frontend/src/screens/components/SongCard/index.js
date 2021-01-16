cd2930e476c84bac5db439e32fc1662c7dde35ff
Melanie Jindali <melaniejindali.dm@gmail.com>
2021-01-15 20:13:36 -0600

    Link albums to spotify

--- slank-official/frontend/src/screens/components/SongCard/index.js
+++ slank-official/frontend/src/screens/components/SongCard/index.js
@@ -1,16 +1,42 @@
 import React from 'react'
 import BB from './Beslic Bash.PNG'
+import QT from './Quarantine Dreams.jpg'
+import TZ from './Time Zone.PNG'
 import { faPlayCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const SongCard = () => {
 	return (
-		<div className='song-card'>
-			<h3>Beslic Bash</h3>
-			<img src={BB} alt='Beslic Bash' />
-			<div className='listen-now'>
-				<FontAwesomeIcon icon={faPlayCircle} />
-				<p>Listen now</p>
+		<div className='song-section'>
+			<div className='song-card'>
+				<h3>Beslic Bash</h3>
+				<img src={BB} alt='Beslic Bash' />
+				<a href='https://open.spotify.com/album/3EXbyHv2yZDxK4oRZFn6sR'>
+					<div className='listen-now'>
+						<FontAwesomeIcon className='play-icon' icon={faPlayCircle} />
+						<p>Listen now</p>
+					</div>
+				</a>
+			</div>
+			<div className='song-card'>
+				<h3>Quarantine Dreams</h3>
+				<img src={QT} alt='Quarantine Dreams' />
+				<a href='https://open.spotify.com/album/05dMcYtjXeskbj7j6mRSeA'>
+					<div className='listen-now'>
+						<FontAwesomeIcon className='play-icon' icon={faPlayCircle} />
+						<p>Listen now</p>
+					</div>
+				</a>
+			</div>
+			<div className='song-card'>
+				<h3>Time Zone</h3>
+				<img src={TZ} alt='Time Zone' />
+				<a href='https://open.spotify.com/album/4pdwnti1MEaRlO2Q0HEsE2'>
+					<div className='listen-now'>
+						<FontAwesomeIcon className='play-icon' icon={faPlayCircle} />
+						<p>Listen now</p>
+					</div>
+				</a>
 			</div>
 		</div>
 	)
